05077606d48c92e615e5746034b2e264bf9d3d97
Bas Boot <github@basboot.nl>
2022-06-15 18:08:59 +0200

    Comments

--- l_system.py
+++ l_system.py
@@ -39,7 +39,7 @@ class LSystem:
 
             new_state = []
             for character in self.state:
-                assert character in self.alphabet, f"Illegal charachter: {character}"
+                assert character in self.alphabet, "Illegal character: {character}"
 
                 # apply rule to variable
                 if character in self.rules:
